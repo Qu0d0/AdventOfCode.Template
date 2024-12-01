2c321c1af02e75f880d7793c89faee31e15f5290
Qu0d0 <130589729+Qu0d0@users.noreply.github.com>
2024-12-01 13:41:51 +0100

    Day01

--- AdventOfCode/Day01.cs
+++ AdventOfCode/Day01.cs
@@ -1,4 +1,6 @@
-﻿namespace AdventOfCode;
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace AdventOfCode;
 
 public class Day01 : BaseDay
 {
@@ -9,7 +11,73 @@ public class Day01 : BaseDay
         _input = File.ReadAllText(InputFilePath);
     }
 
-    public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+    public override ValueTask<string> Solve_1() => new($"{SolveDay1()}");
 
-    public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+    public override ValueTask<string> Solve_2() => new($"{CalculateSimilarityScore()}");
+
+    public long SolveDay1()
+    {
+        long runningTotal = 0;
+        List<long> list1 = new List<long>();
+        List<long> list2 = new List<long>();
+        foreach (string line in _input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries))
+        {
+            string[] locationID = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            list1.Add(long.Parse(locationID[0]));
+            list2.Add((long.Parse(locationID[1])));
+        }
+        list1.Sort();
+        list2.Sort();
+        for (int i = 0; i < list1.Count(); i++)
+        {
+            long a = list1[i];
+            long b = list2[i];
+            if (a > b)
+            {
+                runningTotal += a - b;
+            }
+            else
+            {
+                runningTotal += b - a;
+            }
+
+        }
+        Console.WriteLine(runningTotal);
+        return runningTotal;
+
+    }
+
+    public long CalculateSimilarityScore()
+    {
+        List<long> list1 = new List<long>();
+        List<long> list2 = new List<long>();
+        foreach (string line in _input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries))
+        {
+            string[] locationID = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            //Console.WriteLine(locationID);
+
+            list1.Add(long.Parse(locationID[0]));
+            list2.Add((long.Parse(locationID[1])));
+        }
+
+        long similarityScore = 0;
+
+        for (int i = 0; i < list1.Count(); i++)
+        {
+            long currentNumber = list1[i];
+            long currentNumberCount = 0;
+            for (int j = 0; j < list2.Count(); j++)
+            {
+                if(currentNumber == list2[j])
+                {
+                    currentNumberCount++;
+                }
+            }
+            similarityScore += currentNumberCount * currentNumber;
+        }
+        Console.WriteLine(similarityScore);
+        return similarityScore;
+
+    }
 }
